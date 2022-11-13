d3fb306b2cdc8ad244f671b4d1ea2853f0f75dba
shaonianzhentan <ilovevar@gmail.com>
2022-11-13 14:16:52 +0800

    update

--- 3DES加密解密/3des.js
+++ 3DES加密解密/3des.js
@@ -1,32 +1,35 @@
-const crypto = require('crypto')
+import CryptoJS from 'crypto-js'
 
-class EncryptHelper {
+export default class EncryptHelper {
     constructor(key, iv) {
         key = this.md5(key)
         iv = this.md5(iv).substring(0, 11) + '='
-        this.key = Buffer.from(key, "base64")
-        this.iv = Buffer.from(iv, "base64")
+        this.key = CryptoJS.enc.Base64.parse(key);
+        this.iv = CryptoJS.enc.Base64.parse(iv);
     }
 
     Encrypt(data) {
-        var cipher = crypto.createCipheriv('des-ede3-cbc', this.key, this.iv);
-        var ciph = cipher.update(data, 'utf8', 'base64');
-        ciph += cipher.final('base64');
-        return ciph
+        const encrypted = CryptoJS.TripleDES.encrypt(data, this.key, {
+            iv: this.iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        });
+        return encrypted.toString();
     }
 
     Decrypt(data) {
-        var decipher = crypto.createDecipheriv('des-ede3-cbc', this.key, this.iv);
-        var txt = decipher.update(data, 'base64', 'utf8');
-        txt += decipher.final('utf8');
-        return txt
+        const decrypted = CryptoJS.TripleDES.decrypt(data, this.key, {
+            iv: this.iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }).toString(CryptoJS.enc.Utf8);
+        return decrypted
     }
 
     md5(data) {
-        return crypto.createHash('md5').update(data).digest('hex')
+        return CryptoJS.MD5(data).toString()
     }
 }
-
 
 const helper = new EncryptHelper("123456", "111")
 var oldValue = "13800138000";
